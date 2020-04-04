05213a9616853c15bd14e0d0d33db5dd2b53317d
jaymanale <jaymanale29@gmail.com>
2020-04-04 14:20:36 +0530

    added middleware structure

--- projbackend/app.js
+++ projbackend/app.js
@@ -2,15 +2,27 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
-let app = express();
+const app = express();
 
+// middleware imports
+const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+
+// mongoDB connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log('** DB CONNECTED **'))
   .catch((err) => console.log('Error: ', err));
+
+// Middleware goes here
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 //Listen to express port
 const port = process.env.PORT || 8000;
