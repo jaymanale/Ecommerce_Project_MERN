620cb30420be8e6cfb90ed1113568c3c96cd7cdc
jaymanale <jaymanale29@gmail.com>
2020-04-10 12:28:07 +0530

    added signup and authentication methods

--- my-app/src/user/Signup.js
+++ my-app/src/user/Signup.js
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
 import Base from '../core/Base';
+import { signup } from './../auth/helper/index';
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false,
+  });
+
+  const { name, email, password } = values;
+
+  const handleChange = (fieldName) => (event) => {
+    setValues({ ...values, error: false, [fieldName]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            name: '',
+            email: '',
+            password: '',
+            error: '',
+            success: true,
+          });
+        }
+      })
+      .catch();
+  };
+
   const signUpForm = () => {
     return (
       <div className="row">
@@ -9,16 +44,28 @@ const Signup = () => {
           <form action="">
             <div className="form-group">
               <label className="text-dark"> Name</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                onChange={handleChange('name')}
+              />
             </div>
 
             <div className="form-group">
               <label className="text-dark"> Email</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                onChange={handleChange('email')}
+              />
             </div>
             <div className="form-group">
               <label className="text-dark"> Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                onChange={handleChange('password')}
+              />
             </div>
             <div className="form-group">
               <button className="btn btn-primary btn-block">Submit</button>
